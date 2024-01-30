cd29bcb3b354aaa2417fda70bc30f0204bb10bea
Gavin-Soady <gsoady19@gmail.com>
2024-01-30 22:41:41 +0000

    Add Signup Screen

--- app/src/main/java/org/wit/thegreatfilter/MainActivity.kt
+++ app/src/main/java/org/wit/thegreatfilter/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
@@ -36,9 +39,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -61,8 +68,9 @@ class MainActivity : ComponentActivity() {
 
                     Column(modifier = Modifier.fillMaxSize())
                     {
-                        ShowToolBar()
-                        ShowAddFilter()
+                        //ShowToolBar()
+                        //ShowAddFilter()
+                        SignupPage()
                     }
                     Column(modifier = Modifier.fillMaxSize())
                     {
@@ -236,4 +244,83 @@ fun ShowSupportText(isError : Boolean)
             color = MaterialTheme.colorScheme.error,
         )
     else Text(text = "")
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignupPage() {
+
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                ,horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+
+            val usernameState = remember { mutableStateOf(TextFieldValue()) }
+            val emailState = remember { mutableStateOf(TextFieldValue()) }
+            val passwordState = remember { mutableStateOf(TextFieldValue()) }
+
+            val focus = LocalFocusManager.current
+
+            Image(
+                painter = painterResource(id = R.drawable.filter),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(top = 16.dp)
+                    .padding(8.dp)
+            )
+
+            Text(
+                text = "Signup",
+                modifier = Modifier.padding(8.dp),
+                fontSize = 30.sp,
+                fontFamily = FontFamily.SansSerif
+            )
+
+            OutlinedTextField(
+                value = usernameState.value,
+                onValueChange = { usernameState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Username") })
+
+            OutlinedTextField(
+                value = emailState.value,
+                onValueChange = { emailState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Email") })
+
+            OutlinedTextField(
+                value = passwordState.value,
+                onValueChange = { passwordState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Password") },
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+            Button(
+                onClick = {
+                    focus.clearFocus(force = true)
+
+                },
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text(text = "SIGN UP")
+            }
+
+            Text(
+                text = "Go to login",
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(8.dp)
+
+            )
+        }
+
+    }
+
 }
