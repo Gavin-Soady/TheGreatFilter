0238625a360c65e342e6713c58b06f7c1ee2632f
Gavin-Soady <gsoady19@gmail.com>
2024-01-31 20:37:04 +0000

    Add new screens placeholders in preparation for navigation functionality

---
+++ app/src/main/java/org/wit/thegreatfilter/MatchChatScreen.kt
@@ -0,0 +1,24 @@
+package org.wit.thegreatfilter
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MatchChat() {
+
+    Text(
+        text = "Match Chat Screen",
+        modifier = Modifier.padding(8.dp),
+        fontSize = 30.sp,
+        fontFamily = FontFamily.SansSerif
+    )
+
+
+}
